c8d38172cc6459ad4a8aeea657da56713dbe7c4c
George Jones <gmj@pobox.com>
2017-01-22 11:39:42 -0500

    Clean up package management/loading, se guide-key

--- init.el
+++ init.el
@@ -1,22 +1,23 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
-
 (message "init.el ran")
 
 ; George Jones' ~/.emacs.d/init.el
 
 (setq debug-on-error t) ;; uncomment when there are problems
 
+; https://www.emacswiki.org/emacs/ELPA
 
-; use install-package to instll latest org-plus-contrib
+;   it could be easier just to move package-initialize to another point
+;   during startup so you can (require) ELPA packages; 
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+; Make sure we use the right version of org-mode:
 ;
-; then
-;
-; Load current melpa version of org, not system version
+; This has to happen here (not in george.org loaded later in this file)
+; to make sure we load the latest verson of org, not the system/default
+; version.
 
 (dolist (word  (directory-files (expand-file-name "~/.emacs.d/elpa/") t "^org-plus-contrib-*"))
   ; may not get the right/latest version if there are multiple versions in ~/.emacs.d/elpa/
@@ -25,25 +26,14 @@
 	(setq elisp-dir word)
 	(add-to-list 'load-path word)
 	(message (format "addeded to load path: %s"  elisp-dir)))))
- 
-;(add-to-list 'load-path "~/.emacs.d/elpa/org-plus-contrib-20161102/")
-;(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
-;(add-to-list 'load-path "/usr/share/emacs/site-lisp/notmuch")
 
-;; Load the rest of the packages
-;(package-initialize nil)
-;(setq package-enable-at-startup nil)
 
 ; pull in my main .org init file.  This is where most of the fun is.
 (org-babel-load-file "~/.emacs.d/george.org")
 
 ; because junk I don't want to share in git keeps showing up here
 (setq custom-file "~/elisp-secret/emacs-custom.el")
-; Create path to custom-file if it does not exist
-;unless (file-exists-p custom-file)
-;  (let ((dir (file-name-directory custom-file)))
-;    (unless (file-exists-p dir)
-;              (make-directory dir))))
+
 ; Create empty custom file if it does not exist
 (when (not (file-exists-p custom-file))
   (with-temp-buffer (write-file custom-file)))
